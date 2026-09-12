4b34d84640c68f17f0decb2d922540bb315b95c9
giotadimitroulikavala-dot <giotadimitroulikavala@gmail.com>
2026-09-13 00:05:19 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ export const config = {
 };
 
 export default async function handler(request) {
-  const targetURL = 'https://pub-a0e8b4627e3e475ea4eec5019d5e44b0.r2.dev';
+  const targetURL = 'https://pub-9d8aae48684a4ae2bc496a8821143a4d.r2.dev';
   const newURL = new URL(request.url);
   newURL.hostname = new URL(targetURL).hostname;
 
